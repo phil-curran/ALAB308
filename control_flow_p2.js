dbe8dc78d67a0239389b35e8653397a93bc86aeb
Phil Curran <pecurran@outlook.com>
2024-01-13 13:20:07 -0800

    solution fpr p2.

--- control_flow_p2.js
+++ control_flow_p2.js
@@ -1,6 +1,6 @@
 // constant definitions
 const growthRadius = 5;
-const growthArea = Math.round(Math.PI * growthRadius ** 2 * 10) / 10;
+const growthArea = Math.round(Math.PI * growthRadius ** 2 * 100) / 100;
 const minimumPlantSpace = 0.8;
 
 // variable definitions
@@ -10,21 +10,12 @@ let plantGrowthRate = 2;
 let weekCount = 1;
 
 // utility functions
-const chooseStrategy = (pct) => {
-  if (pct < 50) {
-    console.log(
-      `You should plant more plants, or let existing plants keep growing.`
-    );
-    console.log("");
-  } else if (pct >= 50 && pct <= 80) {
-    console.log(`You should monitor plant growth.`);
-  } else {
-    console.log(`You should prune plants to <= 80% capacity.`);
-  }
-};
-
 const calculatePlantArea = () => {
   return Math.round(updatePlantArea() * 100) / 100;
+};
+
+const calculateNewRadius = () => {
+  return Math.round(Math.sqrt(calculatePlantArea() / Math.PI) * 100) / 100;
 };
 
 const calculateGardenCapacity = () => {
@@ -45,13 +36,17 @@ const growPlants = (weeks) => {
     while (weekCount <= weeks) {
       console.log(`Week ${weekCount}:`);
       console.log(
-        `You ${weekCount === 1 ? "plant" : "now have"} ${plantCount} plants.`
+        `You ${
+          weekCount === 1 ? "start with" : "now have"
+        } ${plantCount} plants.`
       );
       console.log(`They take up ${calculatePlantArea()} square meters.`);
       console.log(
+        `The radius of the new growing area would need to be ${calculateNewRadius()} meters.`
+      );
+      console.log(
         `The plants take up ${calculateGardenCapacity()}% of the growing area.`
       );
-      console.log(chooseStrategy(calculateGardenCapacity()));
       // increment values
       weekCount += 1;
       plantCount *= 2;
@@ -61,4 +56,4 @@ const growPlants = (weeks) => {
   }
 };
 
-growPlants(2);
+growPlants(10);
